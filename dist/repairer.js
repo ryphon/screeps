cf2113b62327dd79ca601f57696d9f2eff9293d4
Dylan Shepard <dylan.shepard@digitalglobe.com>
2021-02-08 16:12:37 -0800

    1000 ticks

--- dist/repairer.js
+++ dist/repairer.js
@@ -40,7 +40,7 @@ var roleRepairer = {
                         hp = targ.hits;
                         max = targ.hitsMax;
                         cur = hp / max;
-                        if (ticks < 200) {
+                        if (ticks < 1000) {
                             if(creep.repair(targ) == ERR_NOT_IN_RANGE) {
                                 creep.moveTo(targ, {visualizePathStyle: {stroke: '#0fffff'}});
                             }
