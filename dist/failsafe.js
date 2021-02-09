ae623634921f3aaa242f551ed53abccc938e3082
Dylan Shepard <dylan.shepard@digitalglobe.com>
2021-02-08 16:28:46 -0800

    only if hostiles

--- dist/failsafe.js
+++ dist/failsafe.js
@@ -3,9 +3,11 @@ module.exports = {
     saveMyRoom: function(myRoomName) {
 
         var walls = Game.rooms[myRoomName].find(FIND_STRUCTURES, {filter: (s) => s.structureType == STRUCTURE_WALL || s.structureType == STRUCTURE_RAMPART});
+        var hostiles = Game.rooms[myRoomName].find(FIND_HOSTILE_CREEPS);
         for (let wall of walls) {
             if (wall.hits <= 100) {
-                Game.rooms[myRoomName].controller.activateSafeMode();
+                if (hostiles.length > 0) {
+                    Game.rooms[myRoomName].controller.activateSafeMode();
             }
         }
     }
